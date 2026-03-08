9bded776ad72bff8e868fa343371ff3d78127e4f
afoncom <afon.com12@gmail.com>
2026-03-08 12:41:49 +0300

    Created screen addCar

--- CarShopAfon/Classes/Screens/AddCar/AddCarScreen.swift
+++ CarShopAfon/Classes/Screens/AddCar/AddCarScreen.swift
@@ -11,6 +11,16 @@ struct AddCarScreen: View {
     @StateObject private var viewModel: AddCarViewModel
     private let presenter: AddCarPresenter
     
+    @State private var brand = ""
+    @State private var model = ""
+    @State private var highway = ""
+    @State private var transmission = ""
+    @State private var engine = ""
+    @State private var bodyStyle = ""
+    @State private var exteriorColor = ""
+    @State private var fuel = ""
+    @State private var imageURL = ""
+    
     init(
         viewModel: AddCarViewModel,
         presenter: AddCarPresenter
@@ -26,16 +36,14 @@ struct AddCarScreen: View {
             case .loading:
                 ProgressView()
             case .loaded:
-                VStack {
-                    Text(L10n.Text.loaded)
-                        .foregroundStyle(Color.textDark)
-                }
+                
+                makeMainViewScreen()
+                
             case .error:
                 Text(L10n.Text.error)
                     .foregroundStyle(Color.textDark)
             }
         }
-        
     }
 }
 
@@ -45,5 +53,51 @@ extension AddCarScreen {
         case loading
         case loaded
         case error
+    }
+}
+
+
+extension AddCarScreen {
+    func makeMainViewScreen() -> some View  {
+        Form {
+            Section("Основное") {
+                TextField("Марка", text: $brand)
+                    .foregroundColor(Color.textDark)
+                TextField("Модель", text: $model)
+                    .foregroundColor(Color.textDark)
+                TextField("Цвет", text: $exteriorColor)
+                    .foregroundColor(Color.textDark)
+            }
+            .foregroundColor(Color.textDark)
+            
+            Section("Характеристики") {
+                TextField("Расход", text: $highway)
+                    .foregroundColor(Color.textDark)
+                TextField("КПП", text: $transmission)
+                    .foregroundColor(Color.textDark)
+                TextField("Двигатель", text: $engine)
+                    .foregroundColor(Color.textDark)
+                TextField("Тип кузова", text: $bodyStyle)
+                    .foregroundColor(Color.textDark)
+                TextField("Топливо", text: $fuel)
+                    .foregroundColor(Color.textDark)
+            }
+            .foregroundColor(Color.textDark)
+            
+            Section("Изображение") {
+                TextField("Ссылка на фото", text: $imageURL)
+                    .foregroundColor(Color.textDark)
+            }
+            .foregroundColor(Color.textDark)
+            
+            Button(action: {}) {
+                Text("Сохранить")
+            }
+            .frame(maxWidth: .infinity)
+            .foregroundColor(Color.textDark)
+        }
+        .scrollContentBackground(.hidden)
+        .background(Color.appBackground)
+        .navigationTitle("Новый автомобиль")
     }
 }
